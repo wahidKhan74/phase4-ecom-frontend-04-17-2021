6b5b415817fb010bab4aad495c8dfe8898eb6ab8
Waheed Khan <wahid.khan74@gmail.com>
2021-04-17 15:24:06 +0000

    angular component setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  // data
   title = 'ecom-frontend';
+  
+  //logic
+  showTitle() {
+    return this.title;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CreateComponent } from './products/create/create.component';
+import { UpdateComponent } from './products/update/update.component';
+import { DetailsComponent } from './products/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    ProductsComponent,
+    UsersComponent,
+    FooterComponent,
+    NavbarComponent,
+    CreateComponent,
+    UpdateComponent,
+    DetailsComponent  ],
   imports: [
     BrowserModule,
     AppRoutingModule
