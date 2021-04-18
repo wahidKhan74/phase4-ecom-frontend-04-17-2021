587b35beb91163d9b5da70a57111b80db0cbcef3
Waheed Khan <wahid.khan74@gmail.com>
2021-04-18 15:44:16 +0000

    template form validation

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -8,14 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   public user : User ={ firstName:"", lastName:"", email:"", role:""};
+  public submitted : boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public submitForm(){
-    console.log(this.user);
+  public submitForm(form:any){
+    // console.log(form);
+    if(form.valid) {
+      this.submitted  = true;
+    } else{
+      this.validateForm(form);
+    }
+    // console.log(this.user);
+  }
+
+  validateForm(form:any){
+    Object.keys(form.controls).forEach(field=>{
+      const control = form.controls[field];
+      control.markAsTouched({ onlySelf : true })
+    })
+  }
+
+  public hasError(field:any){
+    return (field.invalid && field.touched && field.errors);
   }
 }
 
